259c192f35028289e50fe85e49bf411ea4c55255
stepheny12 <136033839+stepheny12@users.noreply.github.com>
2023-06-13 14:43:31 +1200

    Add files via upload

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,11 @@
+# functions go here
+
+#main routine code
+want_instructions = input("Do you want to read the instructions? ")
+
+if want_instructions == "yes":
+    print("Instructions go here")
+elif want_instructions == "no":
+    pass
+else:
+    print("Please answer yes/no")
